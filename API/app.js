a9094799ab68d7c31fc359fa691c4f16fc1429a5
willams192 <wiilhams82@gmail.com>
2023-05-06 11:23:46 -0300

    Adcionando a conexão do banco de dados

--- API/app.js
+++ API/app.js
@@ -1,9 +1,9 @@
 const express = require('express')
 const app = express()
+const routes = require('./Routes/user_route')
 
-app.get('/', (req, res) => {
-  res.send('API funcionando!')
-})
+
+app.use(routes)
 
 app.listen(3000, () => {
   console.log('API rodando na porta 3000')
