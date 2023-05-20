5a709366348bb3b3bbe220db232d236d2f5feed4
willams192 <wiilhams82@gmail.com>
2023-05-20 17:50:15 -0300

    adicionando a rota para excluir com botão

--- lib/aplicação/user_list.dart
+++ lib/aplicação/user_list.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import './users.dart';
 import 'package:provider/provider.dart';
 import '../main.dart';
+import 'api.dart';
 
 class UserList extends StatefulWidget {
   @override
@@ -44,9 +45,13 @@ class _UserListState extends State<UserList> {
                     IconButton(
                       icon: Icon(Icons.delete),
                       color: Colors.red,
-                      onPressed: () {
-                        Provider.of<Users>(context, listen: false)
-                            .remove(users.byIndex(i));
+                      onPressed: () async {
+                        try {
+                          await Api.removeUser(users.byIndex(i).id);
+                          users.remove(users.byIndex(i));
+                        } catch (e) {
+                          print(e);
+                        }
                       },
                     ),
                   ],
