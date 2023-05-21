bd94468453f711cd74eade622284eaaafc4df714
willams192 <wiilhams82@gmail.com>
2023-05-21 18:55:17 -0300

    calocando o useFindAndModify para não reclamar

--- API/app.js
+++ API/app.js
@@ -5,7 +5,7 @@ var express = require('express'),
   mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://127.0.0.1:27017/Funcionarios', { useNewUrlParser: true, useUnifiedTopology: true }).then(
+mongoose.connect('mongodb://127.0.0.1:27017/Funcionarios', { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false }).then(
   () => { console.log('Database is connected') },
   err => { console.log('Can not connect to the database' + err) });
 
